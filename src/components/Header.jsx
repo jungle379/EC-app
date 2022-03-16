e32d1f89768b20531855fc03a1661a92337ef494
jungle379 <jungle379@gmail.com>
2022-03-16 21:02:57 +0900

    ⚡️

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,49 @@
-// import Link from next/link;
+import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+
+const ClerkFeatures = () => (
+  <Link href="/user">
+    <a className="text-blue-600 hover:text-red-500 hover:underline">
+      アカウント管理ページへ
+      {/* <img src="/icons/arrow-right.svg" /> */}
+    </a>
+  </Link>
+);
+
+const SignupLink = () => (
+  <Link href="/sign-up">
+    <a className="">
+      <img src="/icons/user-plus.svg" />
+      <div className="">
+        <img src="/icons/arrow-right.svg" />
+      </div>
+    </a>
+  </Link>
+);
 
 const Header = () => (
   <>
-    <div>ロゴ</div>
+    <div className="flex justify-between mx-10 my-10 h-20">
+      <div>ロゴ</div>
+      <div className="flex justify-end">
+        <div className="pr-5">
+          <SignedIn>
+            <ClerkFeatures />
+          </SignedIn>
+          <SignedOut>
+            <SignupLink />
+          </SignedOut>
+        </div>
+        <div className="mx-5">
+          <SignedOut>
+            <Link href="/sign-in">Sign in</Link>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </div>
   </>
 );
 
