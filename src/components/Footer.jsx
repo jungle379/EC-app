849d7952d1aec08e38922931e3bc710d164868b1
jungle379 <jungle379@gmail.com>
2022-03-21 21:43:15 +0900

    ⚡️ coler_add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import Link from "next/link";
 
 const Footer = () => (
   <>
-    <div className="flex justify-center my-20">
+    <div className="bg-gray-500 h-[80px]">
+      <h1 className="text-white flex justify-center py-[30px]">
+        <Link href="/">
+          <a>トップに戻る</a>
+        </Link>
+      </h1>
+    </div>
+    <div className="bg-gray-700 text-white flex justify-center py-20">
       <Link href="/">
         <a>©️2022</a>
       </Link>
