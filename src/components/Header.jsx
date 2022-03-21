db38d13f0ec5154fa2deb506e27d61573a9836b3
jungle379 <jungle379@gmail.com>
2022-03-21 21:26:32 +0900

    🔥

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,9 @@ import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 import Slide from "./burger";
 
-// const ClerkFeatures = () => (
-//   <Link href="/user">
-//     <a className="text-blue-600 hover:text-red-500 hover:underline">
-//       アカウント管理ページへ
-//     </a>
-//   </Link>
-// );
-
-// const SignupLink = () => (
-//   <Link href="/sign-up">
-//     <a className="">
-//       <img src="/icons/user-plus.svg" />
-//       <div className="">
-//         <img src="/icons/arrow-right.svg" />
-//       </div>
-//     </a>
-//   </Link>
-// );
-
 const Header = () => (
   <>
-    <div className="flex justify-between px-10 my-10 z-10">
+    <div className="bg-gray-200 flex justify-between px-10 py-10 z-10">
       <div>ロゴ</div>
       <div className="flex justify-center">
         <div className="border-2 border-gray-600 mx-20">
@@ -33,14 +14,6 @@ const Header = () => (
           <button>ボタン</button>
         </div>
         <div className="flex justify-end">
-          {/* <div className="pr-5"> */}
-          {/* <SignedIn>
-              <ClerkFeatures />
-            </SignedIn>
-            <SignedOut>
-              <SignupLink />
-            </SignedOut>
-          </div> */}
           <div className="mx-5">
             <SignedOut>
               <Link href="/sign-in">Sign in</Link>
@@ -53,8 +26,10 @@ const Header = () => (
       </div>
     </div>
     <div>
-      <button className="mx-10">Sidevar</button>
-      <Slide className="mx-10 my-10" />
+      <button onClick={Slide} className="mx-10">
+        Sidevar
+      </button>
+      {/* <Slide className="mx-10 my-10" /> */}
     </div>
   </>
 );
