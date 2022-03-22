b57369abbe95227f2da849afe351f21d79c030e5
jungle379 <jungle379@gmail.com>
2022-03-22 13:36:32 +0900

    🎨

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,26 @@ const Footer = () => (
         </Link>
       </h1>
     </div>
-    <div className="bg-gray-700 text-white flex justify-center py-20">
-      <Link href="/">
-        <a>©️2022</a>
-      </Link>
+    <div className="bg-gray-700 text-white py-10">
+      <div>
+        <div className="flex justify-evenly py-5">
+          <h1>test1</h1>
+          <h2>test2</h2>
+          <h3>test3</h3>
+          <h4>test4</h4>
+        </div>
+        <div className="flex justify-evenly py-5">
+          <h1>test1</h1>
+          <h2>test2</h2>
+          <h3>test3</h3>
+          <h4>test4</h4>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Link href="/">
+          <a>©️2022</a>
+        </Link>
+      </div>
     </div>
   </>
 );
