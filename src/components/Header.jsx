fa69dbcd73c180b1e23c2484b535da737b6c9377
jungle379 <jungle379@gmail.com>
2022-03-24 17:15:29 +0900

    🔥

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,70 @@
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
-import Slide from "./burger";
+import { slide as Menu } from "react-burger-menu";
+
+const ClerkFeatures = () => (
+  <Link href="/user">
+    <a className="text-blue-600 hover:text-red-500 hover:underline">
+      アカウント管理ページへ
+    </a>
+  </Link>
+);
+const SignupLink = () => (
+  <Link href="/sign-up">
+    <a className="">
+      <img src="/icons/user-plus.svg" />
+      <div className="">
+        <img src="/icons/arrow-right.svg" />
+      </div>
+    </a>
+  </Link>
+);
+const Slide = (props) => {
+  return (
+    <Menu noOverlay width={"240px"} {...props}>
+      <div className="">
+        <SignedIn>
+          <ClerkFeatures />
+        </SignedIn>
+        <SignedOut>
+          <SignupLink />
+        </SignedOut>
+      </div>
+      <div>
+        <Link href="/" className="">
+          <a>ホームページ</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/page-2" className="">
+          <a>ページ2</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/page-3" className="">
+          <a>ページ3</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/page-4" className="">
+          <a>ページ4</a>
+        </Link>
+      </div>
+    </Menu>
+  );
+};
 
 const Header = () => (
   <>
     <div className="bg-gray-200 flex justify-between px-10 py-10">
       <div>ロゴ</div>
       <div className="flex justify-center">
+        <div>(ジャンルのラジオボタン)</div>
         <div className="border-2 border-gray-600">
           <input className="w-[800px] h-[35px]" type="text" />
         </div>
         <div className="mr-20 border-2 border-gray-500 h-[40px]">
-          <button className="py-[5px]">ボタン</button>
+          <button className="bg-orange-400 h-[37px]">ボタン</button>
         </div>
         <div className="flex justify-end">
           <div className="mx-5">
@@ -29,7 +82,6 @@ const Header = () => (
       <button onClick={Slide} className="mx-10">
         Sidevar
       </button>
-      {/* <Slide className="mx-10 my-10" /> */}
     </div>
   </>
 );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-// import Link from "next/link";
 
 const Home = () => (
   <>
