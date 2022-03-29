f7f6d65c2d527576a43b2d354e60ff7ae330bd87
jungle379 <jungle379@gmail.com>
2022-03-30 08:38:30 +0900

    🚑

---
+++ src/pages/category/books.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Books = () => (
+  <>
+    <Head>
+      <title>書籍</title>
+    </Head>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </>
+);
+
+export default Books;
