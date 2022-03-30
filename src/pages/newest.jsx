453625995693eb2dad253f76697d01b7353fef0c
jungle379 <jungle379@gmail.com>
2022-03-30 13:56:27 +0900

    🔥

--- src/pages/newest.jsx
+++ src/pages/newest.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { Pagination } from "@mantine/core";
 import { useState } from "react";
 
-function Demo() {
+function Paginate() {
   const [activePage, setPage] = useState(1);
   return <Pagination page={activePage} onChange={setPage} total={10} />;
 }
@@ -30,7 +30,7 @@ const News = () => (
       </Link>
     </div>
     <div></div>
-    <Demo />
+    <Paginate />
   </>
 );
 
