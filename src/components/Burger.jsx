10a81a21e588dd81235c6636eb14bd8efab4778a
jungle379 <jungle379@gmail.com>
2022-03-31 17:24:06 +0900

    ⚡️

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -33,54 +33,52 @@ function Hamburger(props) {
   return (
     <div onClick={clickValue}>
       {menuState ? (
-        <>
-          <div>
-            <span className="absolute h-0.5 w-5 bg-black"></span>
-            <span className="absolute mt-1 h-0.5 w-5 bg-black"></span>
-          </div>
-        </>
+        <div>
+          <span className="absolute h-0.5 w-5 bg-black"></span>
+          <span className="absolute mt-1 h-0.5 w-5 bg-black"></span>
+        </div>
       ) : (
         <>
           <div>
             <span className="absolute transform rotate-45 mt-0.5 h-0.5 w-5 bg-black"></span>
             <span className="absolute transform -rotate-45 mt-0.5 h-0.5 w-5 bg-black"></span>
-          </div>
-          <div className="py-10">
-            <div>
-              <SignedIn>
-                <ClerkFeatures />
-              </SignedIn>
-              <SignedOut>
-                <SignupLink />
-              </SignedOut>
-            </div>
-            <div>
-              <Link href="/" className="">
-                <a className="text-blue-600 hover:text-red-500 hover:underline">
-                  ホームページへ
-                </a>
-              </Link>
-            </div>
-            <div>
-              <Link href="/newest" className="">
-                <a className="text-blue-600 hover:text-red-500 hover:underline">
-                  新着商品
-                </a>
-              </Link>
-            </div>
-            <div>
-              <Link href="/category" className="">
-                <a className="text-blue-600 hover:text-red-500 hover:underline">
-                  カテゴリー一覧
-                </a>
-              </Link>
-            </div>
-            <div>
-              <Link href="/" className="">
-                <a className="text-blue-600 hover:text-red-500 hover:underline">
-                  ログアウト
-                </a>
-              </Link>
+            <div className="py-10">
+              <div>
+                <SignedIn>
+                  <ClerkFeatures />
+                </SignedIn>
+                <SignedOut>
+                  <SignupLink />
+                </SignedOut>
+              </div>
+              <div>
+                <Link href="/" className="">
+                  <a className="text-blue-600 hover:text-red-500 hover:underline">
+                    ホームページへ
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/newest" className="">
+                  <a className="text-blue-600 hover:text-red-500 hover:underline">
+                    新着商品
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/category" className="">
+                  <a className="text-blue-600 hover:text-red-500 hover:underline">
+                    カテゴリー一覧
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/" className="">
+                  <a className="text-blue-600 hover:text-red-500 hover:underline">
+                    ログアウト
+                  </a>
+                </Link>
+              </div>
             </div>
           </div>
         </>
