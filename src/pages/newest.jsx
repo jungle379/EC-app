ebb97b3f9024d225dd593867f11076655bbf7544
jungle379 <jungle379@gmail.com>
2022-04-13 17:52:16 +0900

    🔥

--- src/pages/newest.jsx
+++ src/pages/newest.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Link from "next/link";
 import { Pagination } from "@mantine/core";
 import { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Paginate() {
   const [activePage, setPage] = useState(1);
@@ -13,34 +15,41 @@ const News = () => (
     <Head>
       <title>新着商品</title>
     </Head>
-    <div>各ジャンルの新着商品</div>
-    <div>
-      <Link href="../category/news/fashion">
-        <a>ファッション</a>
-      </Link>
+    <Header />
+    <div className="bg-green-100">
+      <div className="text-2xl px-10 py-10 font-bold">各ジャンルの新着商品</div>
+      <div className="px-10">
+        <div className="py-5">
+          <Link href="../category/news/fashion">
+            <a>ファッション</a>
+          </Link>
+        </div>
+        <div className="py-5">
+          <Link href="../category/news/books">
+            <a>書籍</a>
+          </Link>
+        </div>
+        <div className="py-5">
+          <Link href="../category/news/foods">
+            <a>食品</a>
+          </Link>
+        </div>
+        <div className="py-5">
+          <Link href="">
+            <a>音楽</a>
+          </Link>
+        </div>
+        <div className="py-5">
+          <Link href="">
+            <a>玩具</a>
+          </Link>
+        </div>
+      </div>
+      <div className="flex justify-center py-10">
+        <Paginate />
+      </div>
     </div>
-    <div>
-      <Link href="../category/news/books">
-        <a>書籍</a>
-      </Link>
-    </div>
-    <div>
-      <Link href="../category/news/foods">
-        <a>食品</a>
-      </Link>
-    </div>
-    <div>
-      <Link href="">
-        <a>音楽</a>
-      </Link>
-    </div>
-    <div>
-      <Link href="">
-        <a>玩具</a>
-      </Link>
-    </div>
-    <div></div>
-    <Paginate />
+    <Footer />
   </>
 );
 
