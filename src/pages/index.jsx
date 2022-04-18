c6b4f59f94ce2047c99f9ae8d9a885ea1730d617
jungle379 <jungle379@gmail.com>
2022-04-18 12:06:29 +0900

    🔥

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import Hamburger from "../components/Burger";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Lock from "../components/Lock";
-import { Checkbox } from "@mantine/core";
+// import { Checkbox } from "@mantine/core";
 
 const notify = () => toast.success("TOASTを表示");
 
@@ -26,7 +26,7 @@ const Home = () => (
         </div>
         <div className="px-10 py-10 bg-green-100">
           <Lock />
-          <Checkbox label="I agree to sell my privacy" />
+          {/* <Checkbox label="I agree to sell my privacy" /> */}
         </div>
       </div>
     </main>
