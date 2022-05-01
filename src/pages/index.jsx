a92f7a4a28b4ba3565a6714e9dd4f6172acb779d
jungle379 <jungle379@gmail.com>
2022-05-01 15:22:31 +0900

    🐛

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,36 +12,36 @@ export const Home = () => (
     </Head>
     <Header />
     <main className="h-screen bg-green-100">
-      <div className="flex justify-between h-[100px] py-5">
-        <div className="px-10">
+      <div className="flex justify-between h-[100px]">
+        <div className="px-10 py-5">
           <Hamburger />
         </div>
-        <div className="px-10">
+        <div className="px-10 py-5 hover:text-red-500">
           <Link href="/">
             <a>Amazoness_point</a>
           </Link>
         </div>
-        <div className="px-10">
+        <div className="px-10 py-5 hover:text-red-500">
           <Link href="/views">
             <a>閲覧履歴</a>
           </Link>
         </div>
-        <div className="px-10">
+        <div className="px-10 py-5 hover:text-red-500">
           <Link href="/history">
             <a>購入履歴</a>
           </Link>
         </div>
-        <div className="px-10">
+        <div className="px-10 py-5 hover:text-red-500">
           <Link href="/history">
             <a>***</a>
           </Link>
         </div>
-        <div className="px-10">
+        <div className="px-10 py-5 hover:text-red-500">
           <Link href="/history">
             <a>***</a>
           </Link>
         </div>
-        <div className="px-10">
+        <div className="px-10 py-5 hover:text-red-500">
           <Link href="/history">
             <a>***</a>
           </Link>
