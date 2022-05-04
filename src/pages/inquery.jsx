f59d4a42f46ff45219f01f16fcc4ca18493a128f
jungle379 <jungle379@gmail.com>
2022-05-04 21:38:05 +0900

    ⚡️

--- src/pages/inquery.jsx
+++ src/pages/inquery.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Mailer from "../components/Mailform";
+import { Textarea, Select } from "@mantine/core";
 
 const Inquery = () => (
   <>
@@ -10,9 +11,30 @@ const Inquery = () => (
     </Head>
     <Header />
     <div className="h-screen bg-green-100">
-      <div className="text-2xl">お問い合わせ</div>
-      <div></div>
-      <div></div>
+      <div className="text-4xl font-bold px-40 py-20">お問い合わせ</div>
+      <div className="py-5 flex justify-center">
+        <Select
+          className="w-[500px]"
+          label="何についてのお問合せですか？"
+          placeholder="Pick one"
+          data={[
+            { value: "conpany", label: "企業について" },
+            { value: "product", label: "商品について" },
+            { value: "opinion", label: "運営へのご意見" },
+            { value: "other", label: "その他" },
+          ]}
+        />
+      </div>
+      <div className="py-5 flex justify-center">
+        <Textarea
+          className="w-[600px]"
+          placeholder="Your comment"
+          label="Your comment"
+          error=""
+          size="lg"
+          required
+        />
+      </div>
       <div></div>
       <Mailer />
     </div>
