fe798bb8b3c53f604f67aa2739cac971d80cacea
jungle379 <jungle379@gmail.com>
2022-05-05 11:16:21 +0900

    ⚡️ indexへ引越し

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -3,8 +3,6 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Link from "next/link";
 import { client } from "../libs/client";
-import { useState } from "react";
-import { Calendar } from "@mantine/dates";
 
 export const getStaticProps = async () => {
   const data = await client.getList({
@@ -16,44 +14,38 @@ export const getStaticProps = async () => {
   };
 };
 
-const Cart = (props) => {
-  const [value, setValue] = useState(null);
-  return (
-    <>
-      <Head>
-        <title>カート内</title>
-      </Head>
-      <Header />
-      <div className="h-auto bg-green-100">
-        <div className="flex justify-between">
-          <div className="text-4xl font-bold pt-20 px-20">カート</div>
-          <div className="text-xl hover:text-red-500 py-10 px-40">
-            <Link href="/history">
-              <a>購入履歴へ</a>
-            </Link>
-          </div>
-        </div>
-        <div className="px-40 py-10 text-2xl">
-          <p>{`カート内に保存した商品: ${props.totalCount}個`}</p>
-          <ul>
-            {props.contents.map((content) => {
-              return (
-                <li key={content.id}>
-                  <Link href={`cart/${content.id}`}>
-                    <a>{content.title}</a>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className="py-40 flex justify-end px-40">
-          <Calendar value={value} onChange={setValue} />;
+const Cart = (props) => (
+  <>
+    <Head>
+      <title>カート内</title>
+    </Head>
+    <Header />
+    <div className="h-auto bg-green-100">
+      <div className="flex justify-between">
+        <div className="text-4xl font-bold pt-20 px-20">カート</div>
+        <div className="text-xl hover:text-red-500 py-10 px-40">
+          <Link href="/history">
+            <a>購入履歴へ</a>
+          </Link>
         </div>
       </div>
-      <Footer />
-    </>
-  );
-};
+      <div className="px-40 py-10 text-2xl">
+        <p>{`カート内に保存した商品: ${props.totalCount}個`}</p>
+        <ul>
+          {props.contents.map((content) => {
+            return (
+              <li key={content.id}>
+                <Link href={`cart/${content.id}`}>
+                  <a>{content.title}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+    <Footer />
+  </>
+);
 
 export default Cart;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,26 @@
 import Head from "next/head";
-import Link from "next/link";
-import Hamburger from "../components/Burger";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { Calendar } from "@mantine/dates";
+import { useState } from "react";
 
-export const Home = () => (
-  <>
-    <Head>
-      <title>トップページ</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Header />
-    <main className="h-screen bg-green-100"></main>
-    <Footer />
-  </>
-);
+export const Home = () => {
+  const [value, setValue] = useState(null);
+  return (
+    <>
+      <Head>
+        <title>トップページ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="h-screen bg-green-100">
+        <div className="py-40 flex justify-end px-40">
+          <Calendar value={value} onChange={setValue} />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
