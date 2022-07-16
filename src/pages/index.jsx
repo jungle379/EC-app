b4621ba51759886cdc896dfdabdfccb6a57ac573
jungle379 <jungle379@gmail.com>
2022-07-16 18:30:02 +0900

    ⚡️

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import { Calendar } from "@mantine/dates";
 import { useState } from "react";
 import TestCarousel from "../components/Swiper";
+import Lock from "../components/Lock";
 
 const Home = () => {
   const [value, setValue] = useState(null);
@@ -18,8 +19,13 @@ const Home = () => {
         <div className="px-40 py-10">
           <TestCarousel />
         </div>
-        <div className="py-20 px-20 flex justify-end">
-          <Calendar value={value} onChange={setValue} />
+        <div className="py-20 px-20 flex justify-between">
+          <div className="py-40 px-20">
+            <Lock />
+          </div>
+          <div>
+            <Calendar value={value} onChange={setValue} />
+          </div>
         </div>
       </main>
       <Footer />
