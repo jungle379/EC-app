c1a56a771e611ab9d17b79220d0ebc6bd422ee34
jungle379 <jungle379@gmail.com>
2022-07-23 14:34:14 +0900

    🔥

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,12 +37,12 @@ const Header = () => {
             </form>
           </div>
           <div className="flex justify-end">
-            <div className="pt-[5px] w-[80px] mr-5 flex hover:text-red-500">
+            <div className="border-4 border-x-4 border-y-4 pt-[5px] w-[80px] mr-5 flex justify-center hover:text-red-500">
               <Link href="/">
                 <a>注文履歴</a>
               </Link>
             </div>
-            <div className="pt-[5px] w-[80px] flex hover:text-red-500">
+            <div className="pt-[5px] w-[80px] flex justify-center hover:text-red-500">
               <AiOutlineShoppingCart />
               <Link href="/cart">
                 <a>カート</a>
