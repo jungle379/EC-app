c41c8b343e69c009f116e4bf847a1a71aecae66e
jungle379 <jungle379@gmail.com>
2022-08-29 02:17:10 +0900

    🔥

--- src/pages/footer2/credit.jsx
+++ src/pages/footer2/credit.jsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import Lock from "../../components/Lock";
 
 const Credit = () => (
   <>
@@ -12,9 +11,6 @@ const Credit = () => (
       <Header />
       <div className="text-2xl py-20 px-40 font-bold">
         クレジットカードでの支払いについて
-      </div>
-      <div className="px-10 py-10">
-        <Lock />
       </div>
     </div>
     <Footer />
