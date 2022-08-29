e4f4acc7b0e441b8006e3f6832bd6fa4a53ccd41
jungle379 <jungle379@gmail.com>
2022-08-30 05:28:53 +0900

    🐛

--- src/pages/footer2/tob.jsx
+++ src/pages/footer2/tob.jsx
@@ -5,11 +5,11 @@ import Header from "../../components/Header";
 const Tob = () => (
   <>
     <Head>
-      <title>Tob製品群について</title>
+      <title>ToB製品群について</title>
     </Head>
     <div className="bg-green-100 h-auto">
       <Header />
-      <div className="text-2xl py-20 px-40 font-bold">Tob製品群について</div>
+      <div className="text-2xl py-20 px-40 font-bold">ToB製品群について</div>
     </div>
     <Footer />
   </>
