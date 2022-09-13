5fc08d1b775d5875d240bc730d054ae7ea4b1e0f
jungle379 <jungle379@gmail.com>
2022-09-13 12:44:26 +0900

    ⚡️

--- src/pages/user/mypage.jsx
+++ src/pages/user/mypage.jsx
@@ -12,7 +12,7 @@ const Mypage = () => (
     <div className="h-screen">
       <Header />
       <div className="flex justify-between">
-        <div className="text-2xl py-20 px-40 font-bold">マイページ</div>
+        <div className="text-4xl py-20 px-40 font-bold">マイページ</div>
         <div className="py-20 px-20">
           <Link href="/">ユーザ情報編集</Link>
         </div>
@@ -21,21 +21,37 @@ const Mypage = () => (
         <SimpleGrid cols={3}>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">注文履歴</Link>
+            <h1 className="text-sm text-gray-400">
+              配送状況の確認・返品手続き
+            </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">アカウントサービス</Link>
+            <Link href="/">ログインとセキュリティ</Link>
+            <h1 className="text-sm text-gray-400">アカウント情報の変更</h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">プライム</Link>
+            <h1 className="text-sm text-gray-400">
+              会員特典とお支払い方法の確認
+            </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">アドレス帳</Link>
+            <h1 className="text-sm text-gray-400">
+              配送状況の確認・返品手続き
+            </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">支払い方法</Link>
+            <h1 className="text-sm text-gray-400">
+              配送状況の確認・返品手続き
+            </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">ギフト券</Link>
+            <h1 className="text-sm text-gray-400">
+              配送状況の確認・返品手続き
+            </h1>
           </div>
         </SimpleGrid>
       </div>
