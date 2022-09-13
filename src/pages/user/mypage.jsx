eb842fa892513c3b74633826abf2ba3f5db0c258
jungle379 <jungle379@gmail.com>
2022-09-13 16:46:21 +0900

    ⚡️

--- src/pages/user/mypage.jsx
+++ src/pages/user/mypage.jsx
@@ -12,8 +12,8 @@ const Mypage = () => (
     <Header />
     <div className="min-h-screen">
       <div className="flex justify-between">
-        <div className="text-4xl py-20 px-40 font-bold">マイページ</div>
-        <div className="py-20 px-20">
+        <div className="text-4xl py-10 px-40 font-bold">マイページ</div>
+        <div className="py-10 px-20">
           <Link href="/">ユーザ情報編集</Link>
         </div>
       </div>
@@ -37,75 +37,85 @@ const Mypage = () => (
           </div>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">アドレス帳</Link>
-            <h1 className="text-sm text-gray-400">
-              配送状況の確認・返品手続き
-            </h1>
+            <h1 className="text-sm text-gray-400">登録住所の追加・変更</h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">支払い方法</Link>
+            <Link href="/">お客様の支払い方法</Link>
             <h1 className="text-sm text-gray-400">
-              配送状況の確認・返品手続き
-            </h1>
-          </div>
-          <div className="py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
-            <h1 className="text-sm text-gray-400">
-              配送状況の確認・返品手続き
+              支払い方法と設定の管理、全てのトランザクションの表示
             </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
             <Link href="/">ギフト券</Link>
             <h1 className="text-sm text-gray-400">
-              配送状況の確認・返品手続き
+              残高の確認・ギフト券の発行
             </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <Link href="/">メッセージセンター</Link>
+            <h1 className="text-sm text-gray-400">お知らせのEメールを確認</h1>
+          </div>
+          <div className="py-10 border-4 border-gray-200">
+            <Link href="/">カスタマーサービスに連絡</Link>
             <h1 className="text-sm text-gray-400">
-              配送状況の確認・返品手続き
+              お電話またはチャットでお問い合わせ
             </h1>
           </div>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
-            <h1 className="text-sm text-gray-400">
-              配送状況の確認・返品手続き
-            </h1>
+            <Link href="/">モバイルアプリ</Link>
+            <h1 className="text-sm text-gray-400">アプリをダウンロード</h1>
           </div>
         </SimpleGrid>
       </div>
       <div className="border-t-2 my-10 mx-40">
         <SimpleGrid cols={3}>
           <div className="mt-10 py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">デジタルコンテンツとデバイス</Link>
+            </div>
           </div>
           <div className="mt-10 py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">Eメールとメッセージ</Link>
+            </div>
           </div>
           <div className="mt-10 py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">その他の支払い方法</Link>
+            </div>
           </div>
         </SimpleGrid>
       </div>
       <div className="mx-40">
         <SimpleGrid cols={3}>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">お買い物設定</Link>
+            </div>
           </div>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">その他のアカウント</Link>
+            </div>
           </div>
           <div className="py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">お買い物プログラム</Link>
+            </div>
           </div>
         </SimpleGrid>
       </div>
       <div className="mx-40">
         <SimpleGrid cols={2}>
           <div className="my-10 py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">メールマガジン＆その他の設定</Link>
+            </div>
           </div>
           <div className="my-10 py-10 border-4 border-gray-200">
-            <Link href="/">ギフト券</Link>
+            <div className="font-bold">
+              <Link href="/">データとプライバシー</Link>
+            </div>
           </div>
         </SimpleGrid>
       </div>
