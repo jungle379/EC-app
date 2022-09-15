d77c0bb51a5f39e9857d4fdd1469c16ce36c9ffe
jungle379 <jungle379@gmail.com>
2022-09-15 18:32:20 +0900

    🔥

--- src/pages/header2/views.jsx
+++ src/pages/header2/views.jsx
@@ -3,7 +3,6 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { Tabs } from "@mantine/core";
 import { useState } from "react";
-import Lock from "../../components/Lock";
 import { client } from "../../libs/client";
 import Link from "next/link";
 
@@ -26,20 +25,20 @@ const Views = (props) => {
         <title>閲覧履歴</title>
       </Head>
       <Header />
-      <div className="bg-green-100 h-screen">
+      <div className="bg-green-100 min-h-screen">
         <div className="flex justify-center text-4xl py-20 px-40 font-bold">
           閲覧履歴
         </div>
         <div className="px-40 py-10 my-20 mx-20 text-2xl border-white border-4 border-x-4 border-y-4">
           <Tabs active={activeTab} onTabChange={setActiveTab}>
             <Tabs.Tab label="先週">
-              <div>
+              <div className="h-[600px]">
                 <p>{`先週の閲覧履歴: ${props.totalCount}件`}</p>
                 <ul>
                   {props.contents.map((content) => {
                     return (
                       <li className="mt-10" key={content.id}>
-                        <Link href={`views/${content.id}`}>
+                        <Link href={`/views/${content.id}`}>
                           <a>{content.title}</a>
                         </Link>
                       </li>
@@ -48,8 +47,38 @@ const Views = (props) => {
                 </ul>
               </div>
             </Tabs.Tab>
-            <Tabs.Tab label="先月">先月の閲覧履歴</Tabs.Tab>
-            <Tabs.Tab label="半年">半年間の閲覧履歴</Tabs.Tab>
+            <Tabs.Tab label="先月">
+              <div className="h-[600px]">
+                <p>{`先月の閲覧履歴: ${props.totalCount}件`}</p>
+                <ul>
+                  {props.contents.map((content) => {
+                    return (
+                      <li className="mt-10" key={content.id}>
+                        <Link href={`/views/${content.id}`}>
+                          <a>{content.title}</a>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </Tabs.Tab>
+            <Tabs.Tab label="半年">
+              <div className="h-[600px]">
+                <p>{`半年間の閲覧履歴: ${props.totalCount}件`}</p>
+                <ul>
+                  {props.contents.map((content) => {
+                    return (
+                      <li className="mt-10" key={content.id}>
+                        <Link href={`/views/${content.id}`}>
+                          <a>{content.title}</a>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </Tabs.Tab>
           </Tabs>
         </div>
       </div>
