586cf4ce8de5393cac5a737b79027617984fc0b6
jungle379 <jungle379@gmail.com>
2022-09-16 14:51:43 +0900

    ⚡️

--- src/pages/mypage/gift.jsx
+++ src/pages/mypage/gift.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 const Gift = () => {
   return (
@@ -9,27 +9,28 @@ const Gift = () => {
       <Head>
         <title>残高の確認・ギフト券の発行</title>
       </Head>
-      <div className="bg-green-100 h-auto">
-        <Header />
-        <div className="flex justify-between">
-          <div className="text-4xl py-10 px-40 font-bold">
-            残高の確認・ギフト券の発行
+      <Header />
+      <div className="bg-green-100 min-h-screen">
+        <div className="text-4xl py-10 px-40 font-bold">
+          残高の確認・ギフト券の発行
+        </div>
+        <div className="flex justify-evenly">
+          <div className="py-10 text-xl px-20">
+            <Link href="/">
+              <a>残高の確認</a>
+            </Link>
           </div>
-          <div className="flex justify-evenly">
-            <div className="py-10 text-2xl px-20">
-              <Link href="/">
-                <a>残高の確認</a>
-              </Link>
-            </div>
-            <div className="py-10 text-2xl px-20">
-              <Link href="/">
-                <a>ギフト券の発行</a>
-              </Link>
-            </div>
+          <div className="py-10 text-xl px-20">
+            <Link href="/">
+              <a>ギフト券の発行</a>
+            </Link>
           </div>
         </div>
+        <div className="my-10 mx-20 flex justify-end">
+          <Link href="/">Topへ戻る</Link>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
