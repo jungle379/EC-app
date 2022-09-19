05573d7ab81ce375eabb29ccffb2cf478b7115de
jungle379 <jungle379@gmail.com>
2022-09-19 17:47:48 +0900

    ⚡️

--- src/pages/mypage/payment.jsx
+++ src/pages/mypage/payment.jsx
@@ -14,18 +14,6 @@ const Payment = () => {
         <div className="text-4xl py-10 px-40 font-bold">
           お客様の支払い方法について
         </div>
-        <div className="flex justify-evenly">
-          <div className="py-10 text-xl px-20">
-            <Link href="/">
-              <a>残高の確認</a>
-            </Link>
-          </div>
-          <div className="py-10 text-xl px-20">
-            <Link href="/">
-              <a>ギフト券の発行</a>
-            </Link>
-          </div>
-        </div>
         <div className="my-10 mx-20 flex justify-end">
           <Link href="/">Topへ戻る</Link>
         </div>
